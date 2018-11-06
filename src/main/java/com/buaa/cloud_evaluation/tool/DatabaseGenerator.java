a24a96d556ec575069b1878e893678f1047e6305
seven332 <seven332@163.com>
2018-11-06 15:09:10 +0800

    Add db generator

---
+++ src/main/java/com/buaa/cloud_evaluation/tool/DatabaseGenerator.java
@@ -0,0 +1,48 @@
+package com.buaa.cloud_evaluation.tool;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseGenerator {
+
+  public static void main(String[] arg) {
+    File dbFile = new File("src/main/resources/db/cloud_evaluate.db");
+    if (dbFile.exists()) {
+      return;
+    }
+
+    Connection connection = null;
+    try {
+      connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/db/cloud_evaluate.db");
+      Statement statement = connection.createStatement();
+      statement.setQueryTimeout(30);
+
+      statement.executeUpdate("create table nodes ("
+          + "id integer primary key autoincrement,"
+          + "name text,"
+          + "parent integer," // 0 if it's root
+          + "value integer,"
+          + "source integer"
+          + ")");
+
+      statement.executeUpdate("create table node_values ("
+          + "id integer primary key autoincrement,"
+          + "matrix string," // nullable
+          + "vector string" // nullable
+          + ")");
+
+    } catch(SQLException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        if(connection != null)
+          connection.close();
+      } catch(SQLException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
