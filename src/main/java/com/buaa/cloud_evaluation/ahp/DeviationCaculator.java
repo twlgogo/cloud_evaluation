2eafaf7d5247870fc94c3f9cd530f24224b411ae
seven332 <seven332@163.com>
2018-12-03 23:41:15 +0800

    Update DeviationCaculator

--- src/main/java/com/buaa/cloud_evaluation/ahp/DeviationCaculator.java
+++ src/main/java/com/buaa/cloud_evaluation/ahp/DeviationCaculator.java
@@ -1,7 +1,6 @@
 package com.buaa.cloud_evaluation.ahp;
 
 import java.io.File;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
@@ -38,19 +37,37 @@ public class DeviationCaculator {
     return Math.sqrt(dVar/m);
   }
 
+  //标准差σ=sqrt(s^2)
+  public static double StandardDiviation(List<Double> x) {
+    int m=x.size();
+    double sum=0;
+    for(int i=0;i<m;i++){//求和
+      sum+=x.get(i);
+    }
+    double dAve=sum/m;//求平均值
+    double dVar=0;
+    for(int i=0;i<m;i++){//求方差
+      dVar+=(x.get(i)-dAve)*(x.get(i)-dAve);
+    }
+    return Math.sqrt(dVar/m);
+  }
+
   public static void main(String[] args) {
     Double[] data;
     try {
-      File file = new File("/Users/tanweiliang/Desktop/qunluo1Var.txt");
+      File file = new File("qunluo1Var.txt");
       Scanner sc = new Scanner(file);
       List<Double> list = new LinkedList<>();
       while (sc.hasNextLine()) {
         list.add(Double.parseDouble(sc.nextLine()));
       }
+
+
       data = new Double[list.size()];
       for (int i = 0; i < data.length; i++) {
         data[i] = list.get(i);
       }
+
       for (int i = 1; i <= data.length; i++) {
         double[] request = new double[i];
         for (int j = 0; j < i; j++) {
