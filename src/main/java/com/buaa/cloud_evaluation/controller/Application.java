7be6ca3515a7b839b42cec04107aa0b22c0fe351
tanweiliang <twlgogo@163.com>
2018-12-09 21:23:23 +0800

    修改主页显示

--- src/main/java/com/buaa/cloud_evaluation/controller/Application.java
+++ src/main/java/com/buaa/cloud_evaluation/controller/Application.java
@@ -3,11 +3,15 @@ package com.buaa.cloud_evaluation.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 public class Application {
 
     @RequestMapping("/")
-    public static String index(){
+    public static String index(HttpServletResponse response){
+        response.setStatus(302);
+        response.addHeader("Location", "/login.html");
         return "Hello world, by Spring boot";
     }
 }
